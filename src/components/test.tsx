43a79c26a612beb7b5df8c0343408e906b2f2177
YVEvseev <yvevseev@mai.education>
2024-06-29 17:17:32 +0300

    lesson first time

---
+++ src/components/test.tsx
@@ -0,0 +1,25 @@
+export default function Test({title, date, content}: {
+  title: string
+  date: string
+  content: string
+}) {
+  
+  return <div className='w-full h-full flex justify-center items-center'>
+    <div className='h-[300px] w-[300px] bg-amber-200 px-4 py-2 space-y-8'>
+      {/*header block*/}
+      <div className='flex justify-between items-center'>
+        <div className='text-2xl font-bold'>
+          {title}
+        </div>
+        <div className='text-md font-extralight
+         hover:underline hover:cursor-pointer active:text-[100px]'>
+          {date}
+        </div>
+      </div>
+      {/*content block*/}
+      <div>
+        {content}
+      </div>
+    </div>
+  </div>
+}
